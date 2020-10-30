29c9760472215de0ca7b5c5538200a21a06f1e3a
josmorava <josemorales-1234@hotmail.es>
2020-10-30 04:49:32 -0400

    Creando template en JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,32 @@
-const patch = require('path'); //forma de moverse entre archivos
-const HtmlWebpackPlugin = require('html-webpack-plugin');//
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    //entrada- archivo de entrada
-    entry:'./src/index.js',
-    output: { //Direccion donde se copilara todos los resultados
-        path: path.resolve(__dirname, 'dist'),
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
         filename: 'main.js'
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js']
     },
     module:{
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude : /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
             }
-        ]
+        }]
     },
     plugins: [
-        new HtmlWebpackPlugin([
+        new HtmlWebPackPlugin([
             {
-               injecy: true,
-               template: './public/index.html',
-               filename: './index.html' 
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
             }
         ])
     ]
-    
-}
 
+}
